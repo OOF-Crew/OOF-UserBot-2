50d2e16963b26ffaf6badef6bfd4f9cdbf1a6734
MarvynSTAR <71405599+MarvynSTAR@users.noreply.github.com>
2020-09-26 21:27:31 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -23,12 +23,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "100101110"
 @bot.on(dev_cmd(pattern=f"alive", outgoing=True))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running. """
-    await alive.edit("**▬▬▬▬▬ ❴✪❵ SYSTEM ❴✪❵ ▬▬▬▬▬**\n\n"
-                     f"**> Telethon:** {version.__version__}\n"
-                     f"**> Python:** {versions.__python_version__}\n"
-                     f"**> Firmware:** {versions.__version__}\n"
-                     f"**> Licenza:** {versions.__license__}\n"
-                     f"**> Copyright:** {versions.__copyright__}\n"
-                     "**> [UPDATE](https://t.me/IOIIOIIIOIIOI)\n**"
-                     f"**> 👤 USER**: {DEFAULTUSER}\n\n"
+    await alive.edit("**▬▬▬▬▬ ❴✪❵ OOF UserBot ❴✪❵ ▬▬▬▬▬**\n\n"
+                     f"**> Versione Telethon:** {version.__version__}\n"
+                     f"**> Versone Python:** {versions.__python_version__}\n"
+                     f"**> Versione Firmware:** {versions.__version__}\n"
+                     f"**> 👤 UTENTE**: {DEFAULTUSER}\n\n"
                      "▬▬▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬▬▬")
